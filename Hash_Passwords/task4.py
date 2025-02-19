c6ac606917b989f309c025c0c8ecdebbd96e2453
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2025-02-19 11:37:18 +1000

    Complete task 4

--- Hash_Passwords/task4.py
+++ Hash_Passwords/task4.py
@@ -6,3 +6,22 @@ Implement a one-way hash function
 Consider Python’s hashlib module
 You need 2 or 3 players
 """
+import random
+import hashlib
+import struct
+
+password = 'test_passworD#'
+# Encode password to bytes because that is what hashlib expects
+password_bytes = password.encode()
+
+# Generate a random float and convert it to 8-byte representation
+salt = struct.pack("d", random.uniform(0, 1))
+
+hash_function = hashlib.new("sha256")
+#
+hash_function.update(salt + password_bytes)
+
+# Get the final hashed password -  converts the hash output (in bytes) to a readable hexadecimal string
+hashed_password = hash_function.hexdigest()
+
+print(hashed_password)
