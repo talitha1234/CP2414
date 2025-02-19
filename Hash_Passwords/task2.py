4df0becaf2ac3c50399f4f03a07b3116bfe027a7
sethg <seth.gersbach@my.jcu.edu.au>
2025-02-19 14:46:54 +1000

    Working prototype, please review and suggest improvment

--- Hash_Passwords/task2.py
+++ Hash_Passwords/task2.py
@@ -1,4 +1,5 @@
 # code taken from past class, adjusting for task
+from token import NUMBER
 
 # Task 2
 # Read inputs from keyboard typing
@@ -9,47 +10,58 @@
 # You need 2 or 3 players
 
 MIN_LENGTH = 2
-MAX_LENGTH = 6
+MAX_LENGTH = 14
 SPECIAL_CHARS_REQUIRED = False
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
+NUMBER_CHARACTERS = "0123456789"
 
 
 def main():
     """Program to get and check a user's password."""
     print("Please enter a valid password")
     print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
-          "characters, and contain:")
-    print("\t1 or more uppercase characters")
-    print("\t1 or more lowercase characters")
-    print("\t1 or more numbers")
-    if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+          "characters, and contain:", SPECIAL_CHARACTERS)
+
     password = input("> ")
-    while not is_valid_password(password):
-        print("Invalid password!")
-        password = input("> ")
-    print(f"Your {len(password)}-character password is valid: {password}")
+
+    is_valid_password(password)
+
 
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
-    # TODO: if length is wrong, return False
 
-    count_lower = 0
-    count_upper = 0
-    count_digit = 0
-    count_special = 0
-    for char in password:
-        # TODO: count each kind of character (use str methods like isdigit)
-        pass
+    secure = True
+    while secure:
+        length=len(password)
 
-    # TODO: if any of the 'normal' counts are zero, return False
+        if length < MIN_LENGTH or length > MAX_LENGTH:
+            print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH)
+            break
 
-    # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
+        count_lower = 0
+        count_upper = 0
+        count_digit = 0
+        count_special = 0
+        for char in password:
+            if char in SPECIAL_CHARACTERS:
+                count_special += 1
+            if char.isupper():
+                count_upper += 1
+            if char.islower():
+                count_lower += 1
+            if  char in NUMBER_CHARACTERS:
+                count_digit += 1
 
-    # if we get here (without returning False), then the password must be valid
-    return True
+        if count_upper & count_lower & count_digit & count_special >= 1:
+            print('vaild password')
+            break
+        if count_upper or count_lower or count_special or count_digit <= 0:
+            print('invaild password')
+            break
+
+
+        return True
 
 
 main()
