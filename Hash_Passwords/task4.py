d5f652037fc13e0a95c4023c4ba117a489eb4890
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2025-02-19 11:46:47 +1000

    Reformat task 4 to create hash function for use elsewhere

--- Hash_Passwords/task4.py
+++ Hash_Passwords/task4.py
@@ -10,18 +10,20 @@ import random
 import hashlib
 import struct
 
-password = 'test_passworD#'
-# Encode password to bytes because that is what hashlib expects
-password_bytes = password.encode()
+test_password = 'test_passworD#'
 
-# Generate a random float and convert it to 8-byte representation
-salt = struct.pack("d", random.uniform(0, 1))
 
-hash_function = hashlib.new("sha256")
-#
-hash_function.update(salt + password_bytes)
+def hasher(password):
+    # Encode password to bytes because that is what hashlib expects
+    password_bytes = password.encode()
+    # Generate a random float and convert it to 8-byte representation
+    salt = struct.pack("d", random.uniform(0, 1))
+    hash_function = hashlib.new("sha256")
+    #
+    hash_function.update(salt + password_bytes)
+    # Get the final hashed password -  converts the hash output (in bytes) to a readable hexadecimal string
+    hashed_password = hash_function.hexdigest()
+    print("Hashed Password: " + hashed_password)
 
-# Get the final hashed password -  converts the hash output (in bytes) to a readable hexadecimal string
-hashed_password = hash_function.hexdigest()
 
-print(hashed_password)
+hasher(test_password)
